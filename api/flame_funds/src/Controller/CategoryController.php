640de4ae6aeb3ac0ea3b5b2b2c6b7442ff5701e2
Karol Kurowski <karol.123@vp.pl>
2023-10-26 15:17:52 +0200

    add crud for categories

--- api/flame_funds/src/Controller/CategoryController.php
+++ api/flame_funds/src/Controller/CategoryController.php
@@ -4,67 +4,41 @@ namespace App\Controller;
 
 use App\Entity\ExpenseCategory;
 use App\Entity\IncomeCategory;
+use App\Service\Strategy\Category;
+use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * @Route("/api", name="api_")
  */
 class CategoryController extends AbstractController
 {
-    /**
-     * @param EntityManagerInterface $em
-     * @return JsonResponse
-     */
-    #[Route('/category/get-expense', name: 'get_expense_categories', methods: 'GET')]
-    public function getExpenseCategories(EntityManagerInterface $em){
-        $categoryRepository = $em->getRepository(ExpenseCategory::class);
+    private $category;
 
-        $categories = $categoryRepository->findAll();
-
-        $dataToReturn = [];
-        foreach ($categories as $key => $category){
-            if(!$category->isIsDeleted()){
-                $oneRow = [];
-                $oneRow["name"] = $category->getName();
-                $oneRow["details"] = $category->getDetails() ?? "";
-                $dataToReturn[] = $oneRow;
-            }
-        }
-
-        if($dataToReturn){
-            return new JsonResponse($dataToReturn, 200);
-        } else {
-            return new JsonResponse(null, 400);
-        }
-    }
-
-
-    /**
-     * @param EntityManagerInterface $em
-     * @return JsonResponse
-     */
-    #[Route('/category/get-income', name: 'get_income_categories', methods: 'GET')]
-    public function getIncomeCategories(EntityManagerInterface $em): JsonResponse
+    public function __construct(Category $category)
     {
-        $categoryRepository = $em->getRepository(IncomeCategory::class);
+        $this->category = $category;
+    }
 
-        $categories = $categoryRepository->findAll();
+    /**
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    #[Route('/category/{type}', name: 'get_categories', methods: 'GET')]
+    public function getCategories(Request $request, EntityManagerInterface $em, $type): JsonResponse
+    {
+        $user = UserService::getUserFromToken($request, $em);
 
-        $dataToReturn = [];
-        foreach ($categories as $key => $category){
-            if(!$category->isIsDeleted()){
-                $oneRow = [];
-                $oneRow["name"] = $category->getName();
-                $oneRow["details"] = $category->getDetails() ?? "";
-                $dataToReturn[] = $oneRow;
-            }
-        }
+        $categories = $this->category->getAll($type, $user);
 
-        if($dataToReturn){
-            return new JsonResponse($dataToReturn, 200);
+        if($categories){
+            return new JsonResponse($categories, 200);
         } else {
             return new JsonResponse(null, 400);
         }
@@ -73,45 +47,48 @@ class CategoryController extends AbstractController
     /**
      * @param Request $request
      * @param EntityManagerInterface $em
+     * @param $type
+     * @param $id
      * @return JsonResponse
+     * @throws \Exception
      */
-    #[Route('/category/add-expense', name: 'add_expense_category')]
-    public function addExpenseCategory(Request $request, EntityManagerInterface $em){
+    #[Route('/category/{type}/{id}', name: 'get_category', methods: 'GET')]
+    public function getCategory(Request $request, EntityManagerInterface $em, $type, $id): JsonResponse
+    {
+        $category = $this->category->getOneById($type, $id);
+
+        if($category){
+            return new JsonResponse($category, 200);
+        } else {
+            return new JsonResponse(null, 400);
+        }
+    }
+
+    #[Route('/category/{type}/{id}', name: 'edit_category', methods: 'put')]
+    public function editCategory(Request $request, EntityManagerInterface $em, $type, $id){
         $content = $request->getContent();
         $data = json_decode($content, true);
 
-        $newCategory = new ExpenseCategory();
+        $this->category->edit($type, $id, $data['name'], $data['details']);
 
-        $newCategory->setName($data['name']);
-        $newCategory->setDetails($data['details']);
-        $newCategory->setIsDeleted(false);
-
-        $em->persist($newCategory);
-        $em->flush();
-
-        return new JsonResponse("Success saved category", 200);
+        return new Response("Success update");
     }
 
     /**
      * @param Request $request
      * @param EntityManagerInterface $em
      * @return JsonResponse
+     * @throws \Exception
      */
-    #[Route('/category/add-income', name: 'add_income_category')]
-    public function addIncomeCategory(Request $request, EntityManagerInterface $em){
+    #[Route('/category/{type}', name: 'add_expense_category', methods: 'POST')]
+    public function addCategory(Request $request, EntityManagerInterface $em, $type): JsonResponse
+    {
         $content = $request->getContent();
         $data = json_decode($content, true);
 
-        $newCategory = new IncomeCategory();
-
-        $newCategory->setName($data['name']);
-        $newCategory->setDetails($data['details']);
-        $newCategory->setIsDeleted(false);
-
-        $em->persist($newCategory);
-        $em->flush();
+        $user = UserService::getUserFromToken($request, $em);
+        $this->category->add($type,$user, $data['name'], $data['details']);
 
         return new JsonResponse("Success saved category", 200);
     }
-
 }
---
+++ api/flame_funds/src/Service/IncomeCategoryService.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\ExpenseCategory;
+use App\Entity\IncomeCategory;
+use App\Entity\User;
+use App\Service\Interfaces\CategoryInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class IncomeCategoryService implements Interfaces\CategoryInterface
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em){
+        $this->em = $em;
+    }
+    public function addCategory(User $user, string $name, string $details)
+    {
+        $newCategory = new IncomeCategory();
+
+        $newCategory->setUser($user);
+        $newCategory->setName($name);
+        $newCategory->setDetails($details);
+        $newCategory->setIsDeleted(false);
+
+        $this->em->persist($newCategory);
+        $this->em->flush();
+    }
+
+    public function removeCategory(int $categoryId)
+    {
+        $categoryRepository = $this->em->getRepository(IncomeCategory::class);
+        $category = $categoryRepository->find($categoryId);
+
+        $category->setIsDeleted(true);
+
+        $this->em->flush();
+    }
+
+    public function editCategory(int $categoryId, string $newName, string $newDetails)
+    {
+        $categoryRepository = $this->em->getRepository(IncomeCategory::class);
+        $category = $categoryRepository->find($categoryId);
+
+        $category->setName($newName);
+        $category->setDetails($newDetails);
+
+        $this->em->flush();
+    }
+
+    public function getCategory(int $categoryId)
+    {
+        $categoryRepository = $this->em->getRepository(IncomeCategory::class);
+        $category = $categoryRepository->find($categoryId);
+
+        if($category->isIsDeleted()){
+            return false;
+        } else {
+            return ["name" => $category->getName(), "details" => $category->getDetails()];
+        }
+    }
+
+    public function getCategories(User $user)
+    {
+        $categoryRepository = $this->em->getRepository(IncomeCategory::class);
+        $categories =  $categoryRepository->findBy(['user' => $user, "isDeleted" => false]);
+
+        $dataToReturn = [];
+        foreach ($categories as $key => $category){
+            if(!$category->isIsDeleted()){
+                $oneRow = [];
+                $oneRow["name"] = $category->getName();
+                $oneRow["details"] = $category->getDetails() ?? "";
+                $dataToReturn[] = $oneRow;
+            }
+        }
+
+        return $dataToReturn;
+    }
+
+    public function getCategoryType()
+    {
+        return "income";
+    }
+}
