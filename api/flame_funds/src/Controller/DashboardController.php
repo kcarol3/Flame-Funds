c389f9593922b2f21f33997fe73ee2602baace0d
Patrycja Kwaśniewska <kwasniewska.patrycja01@gmail.com>
2023-10-27 02:31:15 +0200

    main_PKW_Periodic DETAILS

--- api/flame_funds/src/Controller/DashboardController.php
+++ api/flame_funds/src/Controller/DashboardController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Account;
 use App\Entity\AccountHistory;
+use App\Entity\Income;
+use App\Entity\IncomeCategory;
+use App\Entity\Periodic;
+use App\Entity\PeriodicDetails;
 use App\Service\DashboardService;
 use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -45,6 +49,64 @@ class DashboardController extends AbstractController
 
         return new JsonResponse($dataToReturn, 200);
     }
+
+    #[Route('/endfinancialgoalcheck', name: 'get_endfinancialgoal_check', methods: 'POST')]
+    public function endFinancialGoalCheck(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $user = UserService::getUserFromToken($request, $em);
+        $accountId = $user->getCurrentAccount();
+        $content = $request->getContent();
+        $data = json_decode($content, true);
+
+        $accountRepository = $em->getRepository(Account::class);
+        $account = $accountRepository->findOneBy(["id" => $accountId]);
+
+        // Pobierz bieżącą datę
+        $today = new \DateTime();
+        $today->setTime(0, 0, 0);
+
+        $periodics = $account->getPeriodics();
+
+        dd($periodics);
+
+        $periodicDetailsRepository = $em->getRepository(PeriodicDetails::class);
+
+        $dataToReturn = [];
+
+        foreach ($periodics as $periodic) {
+            $dateDiff = $today->diff($periodic->getDateStart());
+            $daysBetween = $today->diff($periodic->getDateStart())->days;
+
+            if ($periodic->getIsDeleted() == 0) {
+                for ($i = $daysBetween - $periodic->getDays(); $i >= 0; $i -= $periodic->getDays()) {
+                    $missingDate = clone $periodic->getDateStart();
+                    $missingDate->modify("+$i days");
+
+                    // Dodaj warunek, żeby pomijać datę startową
+                    if ($missingDate != $periodic->getDateStart()) {
+                        $existingDetails = $periodicDetailsRepository->findOneBy([
+                            "date" => $missingDate,
+                            "periodic" => $periodic->getId(),
+                        ]);
+
+                        if (!$existingDetails) {
+                            $periodicDetails = new PeriodicDetails();
+                            $periodicDetails->setDate($missingDate);
+                            $periodicDetails->setAmount($periodic->getAmount());
+                            $periodicDetails->setPeriodic($periodic);
+
+                            $em->persist($periodicDetails);
+                            $dataToReturn[] = $periodicDetails;
+                        }
+                    }
+                }
+            }
+        }
+        $em->flush();
+
+        return new JsonResponse($dataToReturn, 200);
+    }
+
 
     #[Route('/history', name: 'get_history_data', methods: 'GET')]
     public function getHistory(Request $request, EntityManagerInterface $em): JsonResponse
